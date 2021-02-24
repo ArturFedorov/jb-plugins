0ad8a8dd4e902f50d9f9269a0a43add2ad6201a0
Artur Fedorov <artur.fodorov@gmail.com>
2021-02-24 20:31:00 +0300

    JB 03: Test and pipeline

--- src/pages/plugins/PluginSearchResults.tsx
+++ src/pages/plugins/PluginSearchResults.tsx
@@ -2,7 +2,6 @@ import React, { FunctionComponent } from 'react';
 import { useLocation } from 'react-router-dom';
 
 const PluginSearchResultsPage: FunctionComponent = () => {
-  console.log(useLocation().search);
   return <div>Search {useLocation().search}</div>;
 };
 
