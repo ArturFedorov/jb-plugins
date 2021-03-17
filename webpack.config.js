e36bfa78387b79fd342bb735b269bfe30cf47178
ArturFedorov <artur.fodorov@gmail.com>
2021-03-17 16:24:14 +0300

    styling fix

--- webpack.config.js
+++ webpack.config.js
@@ -4,124 +4,126 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const svgToMiniDataURI = require('mini-svg-data-uri');
 const webpack = require('webpack');
 
-const isEnvDevelopment = process.env.NODE_ENV === 'development';
-const isEnvProduction = process.env.NODE_ENV === 'production';
+module.exports = (env, argv) => {
+  process.env.NODE_ENV = argv.mode;
+  const isEnvDevelopment = argv.mode === 'development';
+  const isEnvProduction = argv.mode === 'production';
 
-module.exports = {
-  mode: isEnvDevelopment ? 'development' : 'production',
-  entry: './src/index.tsx',
-  output: {
-    filename: '[name].[chunkhash].js',
-    path: path.resolve(__dirname, 'build'),
-    clean: true
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx', '.scss']
-  },
-  optimization: {
-    splitChunks: {
-      chunks: 'all'
-    }
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx|ts|tsx)$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader'
-          }
-        ]
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader'
-          }
-        ]
-      },
-      {
-        test: /\.(png|jpe?g|gif)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: 'public/static/media/[name].[hash:8].[ext]'
+  return {
+    mode: argv.mode,
+    entry: './src/index.tsx',
+    output: {
+      filename: '[name].[chunkhash].js',
+      path: path.resolve(__dirname, 'build'),
+      clean: true
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.jsx', '.scss']
+    },
+    optimization: {
+      splitChunks: {
+        chunks: 'all'
+      }
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx|ts|tsx)$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'babel-loader'
             }
-          }
-        ]
-      },
-      {
-        test: /\.svg$/i,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              generator: (content) => svgToMiniDataURI(content.toString())
+          ]
+        },
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: 'html-loader'
             }
-          }
-        ]
-      },
-      {
-        test: /\.module\.s(a|c)ss$/,
-        use: [
-          isEnvDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              modules: {
-                exportLocalsConvention: 'camelCaseOnly'
+          ]
+        },
+        {
+          test: /\.(png|jpe?g|gif)$/i,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: 'public/static/media/[name].[hash:8].[ext]'
               }
             }
-          },
-          'resolve-url-loader',
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: isEnvDevelopment
-            }
-          }
-        ]
-      },
-      {
-        test: /\.s(a|c)ss$/,
-        exclude: /\.module.(s(a|c)ss)$/,
-        use: [
-          isEnvDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
-          'css-loader',
-          'resolve-url-loader',
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: isEnvDevelopment
-            }
-          }
-        ]
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        exclude: /node_modules/,
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]'
-        }
-      }
-    ]
-  },
-  devtool: 'inline-source-map',
-  plugins: [
-    new HtmlWebpackPlugin(
-      // eslint-disable-next-line prefer-object-spread
-      Object.assign(
-        {},
-        {
-          inject: true,
-          template: path.resolve(__dirname, 'public/index.html')
+          ]
         },
-        isEnvProduction
-          ? {
+        {
+          test: /\.svg$/i,
+          use: [
+            {
+              loader: 'url-loader',
+              options: {
+                generator: (content) => svgToMiniDataURI(content.toString())
+              }
+            }
+          ]
+        },
+        {
+          test: /\.module\.s(a|c)ss$/,
+          use: [
+            isEnvDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+            {
+              loader: 'css-loader',
+              options: {
+                modules: {
+                  exportLocalsConvention: 'camelCaseOnly'
+                }
+              }
+            },
+            'resolve-url-loader',
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: isEnvDevelopment
+              }
+            }
+          ]
+        },
+        {
+          test: /\.s(a|c)ss$/,
+          exclude: /\.module.(s(a|c)ss)$/,
+          use: [
+            isEnvDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
+            'css-loader',
+            'resolve-url-loader',
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: isEnvDevelopment
+              }
+            }
+          ]
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          exclude: /node_modules/,
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]'
+          }
+        }
+      ]
+    },
+    devtool: 'inline-source-map',
+    plugins: [
+      new HtmlWebpackPlugin(
+        // eslint-disable-next-line prefer-object-spread
+        Object.assign(
+          {},
+          {
+            inject: true,
+            template: path.resolve(__dirname, 'public/index.html')
+          },
+          isEnvProduction
+            ? {
               minify: {
                 removeComments: true,
                 collapseWhitespace: true,
@@ -135,27 +137,28 @@ module.exports = {
                 minifyURLs: true
               }
             }
-          : undefined
-      )
-    ),
-    new MiniCssExtractPlugin({
-      filename: isEnvDevelopment ? '[name].css' : '[name].[hash].css',
-      chunkFilename: isEnvDevelopment ? '[id].css' : '[id].[hash].css'
-    }),
-    new webpack.ProvidePlugin({
-      process: 'process/browser'
-    })
-  ],
-  devServer: {
-    port: 5000,
-    historyApiFallback: true,
-    contentBase: './src',
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        pathRewrite: { '^/api': '' }
+            : undefined
+        )
+      ),
+      new MiniCssExtractPlugin({
+        filename: isEnvDevelopment ? '[name].css' : '[name].[hash].css',
+        chunkFilename: isEnvDevelopment ? '[id].css' : '[id].[hash].css'
+      }),
+      new webpack.ProvidePlugin({
+        process: 'process/browser'
+      })
+    ],
+    devServer: {
+      port: 5000,
+      historyApiFallback: true,
+      contentBase: './src',
+      proxy: {
+        '/api': {
+          target: 'http://localhost:8080',
+          changeOrigin: true,
+          pathRewrite: { '^/api': '' }
+        }
       }
     }
-  }
+  };
 };
